bb6316e7563bab63cdc5db755be9e0ac259dd6fc
Guillon <guillonangele@gmail.com>
2017-05-10 09:45:16 +0200

    adherent -> famille et enfant

--- Aperov2/webdev/vues/adherent/v_adherent.php
+++ Aperov2/webdev/vues/adherent/v_adherent.php
@@ -1,10 +1,16 @@
 <div class="content">
     <div class="row">
-        <div class="col-12">
-            <h1 class="text-center">Adherent</h1>
-            <button class="button-primary" type="button">Ajouter adhérent</button>
-            <button class="button-primary" type="button">Supprimer adhérent</button>
-            <button class="button-primary" type="button">Modifier adhérent</button>
+        <div class="col-4">
+            <h1 class="text-center">Famille</h1>
+            <button class="button-primary" type="button">Ajouter Famille</button>
+            <button class="button-primary" type="button">Supprimer Famille</button>
+            <button class="button-primary" type="button">Modifier Famille</button>
+        </div>
+        <div class="col-4">
+            <h1 class="text-center">Enfant</h1>
+            <button class="button-primary" type="button">Ajouter Enfant</button>
+            <button class="button-primary" type="button">Supprimer Enfant</button>
+            <button class="button-primary" type="button">Modifier Enfant</button>
         </div>
     </div>
 </div>
